c35ef935cb2ef990318089ec0c2bc772d386f2c5
FLX-X <fhernan@live.cl>
2023-11-23 16:16:17 -0300

    add shortUrl DB model

---
+++ models/shortUrl.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid';
+
+const shortUrlSchema = new mongoose.Schema({
+  full: { type: String, required: true },
+  short: { type: String, required: true, default: nanoid(6) },
+  clicks: { type: Number, required: true, default: 0 },
+  createdAt: { type: String, default: new Date() },
+});
+
+export default mongoose.model('ShortUrl', shortUrlSchema);
