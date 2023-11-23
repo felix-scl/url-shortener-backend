b0d26ae94e53441fe47a30d665961615849d1e3c
FLX-X <fhernan@live.cl>
2023-11-23 16:16:45 -0300

    add server.js complete functionality

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import ShortUrl from './models/shortUrl.js';
+
+const app = express();
+const PORT = process.env.PORT || 8800;
+
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortURLs = await ShortUrl.find();
+  res.json(shortURLs);
+});
+
+app.post('/shortenUrl', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.json({ ok: true });
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortURL = await ShortUrl.findOne({ short: req.params.shortUrl });
+
+  if (shortURL == null) return res.sendStatus(404);
+  shortURL.clicks++;
+  shortURL.save();
+
+  res.redirect(shortURL.full);
+});
+
+app.listen(PORT, console.log(`Connected to port ${PORT}`));
