4f85b57b89e1229a02cb2addd3afb679fc39a461
FLX-X <fhernan@live.cl>
2023-12-06 15:11:53 -0300

    fix shortUrlSchema types and default functions

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -3,9 +3,9 @@ import { nanoid } from 'nanoid';
 
 const shortUrlSchema = new mongoose.Schema({
   originalUrl: { type: String, required: true },
-  shortId: { type: String, required: true, default: nanoid(6) },
+  shortId: { type: String, required: true, default: () => nanoid(6) },
   clicks: { type: Number, required: true, default: 0 },
-  createdAt: { type: String, default: new Date() },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model('ShortUrl', shortUrlSchema);
